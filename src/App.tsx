d0794621299cb5131711c9624ec6c4dc13a45844
WangCheng4934 <493475850@qq.com>
2021-08-26 00:13:42 +0800

    fix format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { ProjectListScreen } from './views/project-list';
+import React from "react";
+import "./App.css";
+import { ProjectListScreen } from "./views/project-list";
 
 function App() {
   return (
--- src/views/project-list/search-pannel.jsx
+++ src/views/project-list/search-pannel.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React from "react";
 
-export const SearchPannel = ({ param, setParam, users }) => { 
-  
-  return <form>
-    <div>
-      <input type="text" value={param.name} onChange={event => setParam({
-        ...param,
-        name: event.target.value
-      })} />
-      <select value={param.personId} onChange={event => {
-        console.log(event.target.value)
-        setParam({
-          ...param,
-          personId: event.target.value
-        })
-      }}>
-        <option>负责人</option>
-        {
-          users.map(user => <option key={ user.id } value={ user.id }>{ user.name }</option>)
-        }
-      </select>
-    </div>
-  </form>
-}
+export const SearchPannel = ({ param, setParam, users }) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              name: event.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(event) => {
+            console.log(event.target.value);
+            setParam({
+              ...param,
+              personId: event.target.value,
+            });
+          }}
+        >
+          <option>负责人</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};

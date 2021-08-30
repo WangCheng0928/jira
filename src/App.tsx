37a9ecfb7208909ae3dc6606f0c6f94018032955
WangCheng4934 <493475850@qq.com>
2021-08-31 01:11:18 +0800

    feat add loginform && json-server middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import { ProjectListScreen } from "./views/project-list";
+import { LoginForm } from "./views/login/login";
 
 // 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
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginForm />
     </div>
   );
 }
